864ad7755582d62367576ae842bd30a5eb56f247
ParthPali <parthpaliwal2005@gmail.com>
2025-02-07 16:40:44 -0800

    Edit Routine.java

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Routine.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Routine.java
@@ -1,26 +1,40 @@
 package edu.ucsd.cse110.habitizer.lib.domain;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.List;
 
 public class Routine {
-    private List<Task> tasks;
-    private DataRoutine data;
-    private TimeTracker timeTracker;
+
+    private final @NonNull List<Task> tasks;
+    private final @NonNull DataRoutine data;
+    private final @NonNull TimeTracker timeTracker;
+    private final @NonNull Time time;
+
+
+    public Routine(@Nullable List<Task> tasks, @Nullable DataRoutine data, @Nullable TimeTracker timeTracker){
+        this.tasks = tasks;
+        this.data = data;
+        this.timeTracker = timeTracker;
+    }
+
 
     public void start(){
-
+        timeTracker.start();
     }
 
     public void end(){
-
+        timeTracker.stop();
     }
 
     public void setTimeManager(){
-
+        time = timeTracker.elapsedTime();
     }
 
-    public void checkOff(Task task){
-
+    public void checkOffById(Task task){
+        task.setTime(timeTracker.getCheckOffTime());
+        task.setCheckedOff();
     }
 
     public int size(){
