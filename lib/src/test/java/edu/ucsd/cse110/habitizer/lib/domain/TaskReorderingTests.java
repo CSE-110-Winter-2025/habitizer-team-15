bed3b7d2259fced5832db62245dffa0f7252e470
Daniel Bravo <dantpp11@gmail.com>
2025-03-11 16:23:23 -0700

    task reorder BDD test and unit tests

---
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/TaskReorderingTests.java
@@ -0,0 +1,65 @@
+package edu.ucsd.cse110.habitizer.lib.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.ucsd.cse110.habitizer.lib.data.InMemoryDataSource;
+import edu.ucsd.cse110.habitizer.lib.domain.time.MockITimeManager;
+import edu.ucsd.cse110.habitizer.lib.domain.time.TimeTracker;
+
+
+public class TaskReorderingTests {
+
+    //Scenario 1: Change task order
+    //  Given
+    //    User is viewing morning routine
+    //    “Shower” task exists first
+    //    “Brush Teeth” task exists second
+    //  When
+    //    The user taps up arrow to move Brush teeth up
+    //  Then
+    //    The morning routine now displays
+    //    “Brush Teeth” task first
+    //    “Shower” second
+    @Test
+    public void testScenario1() {
+        MockITimeManager mockTime = new MockITimeManager();
+        Routine routine = new Routine(InMemoryDataSource.DATA_MORNING_ROUTINE,
+                new TimeTracker(mockTime));
+
+        int showerId = 0;
+        int brushTeethId = 1;
+        int dressId = 2;
+
+        Task shower = routine.findTaskById(showerId);
+        Task brush = routine.findTaskById(brushTeethId);
+        Task dress = routine.findTaskById(dressId);
+
+        // check start order
+        assertEquals(routine.getTasksSubject().getValue().get(0), shower);
+        assertEquals(routine.getTasksSubject().getValue().get(1), brush);
+        assertEquals(routine.getTasksSubject().getValue().get(2), dress);
+
+        // call moveTaskUp
+        routine.moveTaskUp(brush);
+
+        // check order after moving brush up
+        assertEquals(routine.getTasksSubject().getValue().get(0), brush);
+        assertEquals(routine.getTasksSubject().getValue().get(1), shower);
+        assertEquals(routine.getTasksSubject().getValue().get(2), dress);
+
+        // call moveTaskDown
+        routine.moveTaskDown(brush);
+
+        // check order after moving brush back down
+        assertEquals(routine.getTasksSubject().getValue().get(0), shower);
+        assertEquals(routine.getTasksSubject().getValue().get(1), brush);
+        assertEquals(routine.getTasksSubject().getValue().get(2), dress);
+
+    }
+}
